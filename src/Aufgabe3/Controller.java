fa7c3bddac3cdee2ed696cd396a9776cc11d8378
LOHIL <LOHIL@HLDEHKN794.hochland.com>
2020-09-10 16:13:06 +0200

    ...

--- src/Aufgabe3/Controller.java
+++ src/Aufgabe3/Controller.java
@@ -1,5 +1,22 @@
 package Aufgabe3;
 
-public class Controller {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class Controller implements Initializable{
+
+		File file;
+		FileChooser fileChooser;
+		
+	public void initialize(URL location, ResourceBundle resources) {
+		fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Textdateien", "*.txt"));
+		
+	}
 
 }
