b15dc9d6f56f9de26c0074d69215f559ea1c65bd
LOHIL <LOHIL@HLDEHKN794.hochland.com>
2020-09-10 16:05:18 +0200

    ...

--- src/Aufgabe3/Exe301.java
+++ src/Aufgabe3/Exe301.java
@@ -1,10 +1,28 @@
 package Aufgabe3;
 
-public class Exe301 {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Exe301 extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
+
+	
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource(View.fxml));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Figgerei");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	
 
 }
