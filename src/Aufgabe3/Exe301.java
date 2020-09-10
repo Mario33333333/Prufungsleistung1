108fb22893a82f25bad7700c086ad95cc01373b2
LOHIL <LOHIL@HLDEHKN794.hochland.com>
2020-09-10 16:04:34 +0200

    ...

--- src/Aufgabe3/Exe301.java
+++ src/Aufgabe3/Exe301.java
@@ -1,10 +1,10 @@
-package MrBizepsInTheHood;
+package Aufgabe3;
 
-public class Marioboy {
+public class Exe301 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Figgerei");
+
 	}
 
 }
