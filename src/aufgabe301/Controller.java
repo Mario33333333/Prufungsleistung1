369e105169d636b6a297cd300d50227518075421
LOHIL <LOHIL@HLDEHKN794.hochland.com>
2020-09-10 17:17:24 +0200

    ...

---
+++ src/aufgabe301/Controller.java
@@ -0,0 +1,25 @@
+package aufgabe301;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class Controller implements Initializable{
+
+	File file;
+	FileChooser fileChooser;
+	
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Textdateien", "*.txt"));
+		
+	}
+	
+	
+
+}
