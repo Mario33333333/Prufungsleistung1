6790ff7ea26eabbc39392441c5588705c73c3af7
LOHIL <LOHIL@HLDEHKN794.hochland.com>
2020-09-10 17:27:39 +0200

    ...

--- src/aufgabe301/Controller.java
+++ src/aufgabe301/Controller.java
@@ -1,12 +1,17 @@
 package aufgabe301;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
 
 public class Controller implements Initializable{
 
@@ -20,6 +25,28 @@ public class Controller implements Initializable{
 		
 	}
 	
+	public void newFile(ActionEvent event) {
+		file = null;
+		textArea.setText("");
+		label.setText("");
+	}
 	
+	public void saveFile(ActionEvent event) {
+		if (file != null) {
+			try(FileWriter fW = new FileWriter(file); BufferedWriter bW = new BufferedWriter(fW)) {
+				bW.write(textArea.getText());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	public void saveFileAs(ActionEvent event) {
+		Window window = menuBar.getScene().getWindow();
+		file = fileChooser.showSaveDialog(window);
+		if (file != null) {
+			
+		}
+	}
 
 }
